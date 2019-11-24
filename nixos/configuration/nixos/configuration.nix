6d866890fb6172ae1596cb6023dcb51a576745fd
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-11-24 02:45:35 -0500

    Updated nixos configuration

--- nixos/configuration/nixos/configuration.nix
+++ nixos/configuration/nixos/configuration.nix
@@ -8,5 +8,35 @@
   virtualisation.docker.enable = true;
   users.groups.docker.members = [ "vagrant" ];
 
-  services.xserver.desktopManager.gnome3.enable = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+
+  services.fractalart.enable = true;
+
+  services.xserver = {
+    enable = true;
+
+    resolutions = [
+     {
+       x = 1920;
+       y = 1080;
+     }
+    ];
+
+    desktopManager = {
+      gnome3.enable = true;
+    };
+
+    displayManager.gdm.enable = true;
+
+    windowManager.awesome.enable = true;
+    
+  };
+  
+  environment.systemPackages = [
+    pkgs.vivaldi
+    pkgs.tilix
+  ];
 }
