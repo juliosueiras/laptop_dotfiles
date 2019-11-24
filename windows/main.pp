137b965ba8dd776460bc10a79b8bacb7ac0be48e
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-11-23 20:42:09 -0500

    Added some new config

--- windows/main.pp
+++ windows/main.pp
@@ -1,6 +1,39 @@
 include chocolatey
 
+Package {
+  ensure => latest,
+  provider => 'chocolatey',
+}
+
+reboot {'dsc_reboot':
+  when => pending,
+}
+
+# Utilities
 package {["Virtualbox", "git", "Steam"]:
-  provider  => chocolatey,
-  ensure    => latest, 
+}
+
+# HashiCorp's Tools
+package {["terraform", "packer", "vagrant"]:
+}
+
+# Packages Graveyard
+package {["mongoclient", "mongodb.portable"]:
+  ensure => 'absent',
+}
+
+
+package {'powershell':}->
+
+# Install WSL(Linux in Windows)
+dsc {'WSL':
+  resource_name => 'WindowsOptionalFeature',
+  module        => {
+    name    => 'PSDesiredStateConfiguration',
+    version => '1.1'
+  },
+  properties => {
+    ensure => 'Enable',
+    name => 'Microsoft-Windows-Subsystem-Linux',
+  }
 }
