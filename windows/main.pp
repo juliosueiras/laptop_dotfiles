51c37830e79eda9fa34366b96853d3aa0c3c0bb2
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-11-23 21:42:47 -0500

    Added remap caps lock to escape

--- windows/main.pp
+++ windows/main.pp
@@ -37,3 +37,14 @@ dsc {'WSL':
     name => 'Microsoft-Windows-Subsystem-Linux',
   }
 }->package {'wsl-alpine': }
+
+
+dsc_xregistry {'remap caps lock to escape':
+  dsc_ensure => 'Present',
+  dsc_key => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout',
+  dsc_valuename => 'Scancode Map',
+  dsc_valuedata => '0000000000000000030000003a00460001003a0000000000',
+  dsc_valuetype => 'Binary',
+  dsc_hex => true,
+  notify => Reboot['dsc_reboot'],
+}
