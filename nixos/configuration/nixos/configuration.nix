ff2beffb35e219cb6f475384bd799c65159a8661
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-12-08 20:26:24 -0500

    Updated

--- nixos/configuration/nixos/configuration.nix
+++ nixos/configuration/nixos/configuration.nix
@@ -45,6 +45,12 @@ in {
 
   nix.binaryCachePublicKeys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "laptop-dotfiles.cachix.org-1:Okurdv1/SRJkXNODJsAi/K6FZLDmhr4gMSwV9XiWw+g=" ];
 
+  nix.trustedUsers = [ "root" "vagrant" ];
+
+  nix.extraOptions = ''
+  substituters = https://cache.nixos.org https://laptop-dotfiles.cachix.org https://cache.nixos.org/
+  '';
+
   nixpkgs.config = {
     allowUnfree = true;
 
